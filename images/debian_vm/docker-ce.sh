eaa37c16277d5c0358daa00f7502591ce9e3f7b1
Cru1zzz3 <udartzev_sv@mail.ru>
2021-02-10 18:05:15 -0500

    Fix typo in docker-ce.sh script

--- images/debian_vm/docker-ce.sh
+++ images/debian_vm/docker-ce.sh
@@ -9,7 +9,7 @@ curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
 sudo add-apt-repository \
    "deb [arch=amd64] https://download.docker.com/linux/debian \
    $(lsb_release -cs) \
-   stablei"
+   stable"
 sudo apt-get update
 sudo apt-get install docker-ce docker-ce-cli containerd.io
 sudo groupadd docker
