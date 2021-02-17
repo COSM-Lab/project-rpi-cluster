d89d62f23a075c09504061da5124ccb43e7db8fb
Cru1zzz3 <udartzev_sv@mail.ru>
2021-02-17 12:08:04 -0500

    Edit dhcpcd.conf in image

--- images/pi-gen/stage2/02-net-tweaks/02-dhcpcd-configuration.sh
+++ images/pi-gen/stage2/02-net-tweaks/02-dhcpcd-configuration.sh
@@ -1,8 +1,8 @@
-cat << 'EOF' >> "${ROOTFS_DIR}/etc/dhcpcd.conf"
+# cat << 'EOF' >> "${ROOTFS_DIR}/etc/dhcpcd.conf"
 
-interface eth0
-nodhcp
-static ip_address=100.100.100.99/24
-static routers=192.168.88.1
-static domain_name_servers=192.168.88.1 8.8.8.8
-EOF
+# interface eth0
+# nodhcp
+# static ip_address=100.100.100.99/24
+# static routers=192.168.88.1
+# static domain_name_servers=192.168.88.1 8.8.8.8
+# EOF
